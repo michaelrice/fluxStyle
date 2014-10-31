71e94538dd5ba860dadd355e5d9ff40156661df8
Michael Rice <michael.rice@rackspace.com>
2014-10-31 09:30:54 -0500

    switch from print to print() in setup. for issue #2

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@
 # manager. Orignal version written by Michael Rice. Many special
 # thanks to Zan a.k.a. Lauri Peltonen for GUI Improvements & Bug Stomping.
 
+from __future__ import print_function
+
 from distutils.core import setup
 import distutils.fancy_getopt
 import sys
 
-DATADIR="/usr/share"
-BINDIR="/usr/bin"
+DATADIR = "/usr/share"
+BINDIR = "/usr/bin"
 
 
 def chk_install():
@@ -19,43 +21,47 @@ def chk_install():
     try:
         import gtk
     except:
-        print "You seem to be missing gtk bindings for python"
-        print "Please install them before you install fluxstyle"
-        print "http://pygtk.org/"
+        print("You seem to be missing gtk bindings for python")
+        print("Please install them before you install fluxstyle")
+        print("http://pygtk.org/")
         raise SystemExit
     try:
         import gtk.glade
     except:
-        print "You need to install libglade2"
+        print("You need to install libglade2")
         raise SystemExit
-    if gtk.pygtk_version < (2,3,90):
-        print "PyGtk 2.3.90 or later required for this program"
-        print "It is reccomended that you get pygtk 2.6 or newer."
+    if gtk.pygtk_version < (2, 3, 90):
+        print("PyGtk 2.3.90 or later required for this program")
+        print("It is reccomended that you get pygtk 2.6 or newer.")
         raise SystemExit
 
 
 def main():
     chk_install()
     doclines = __doc__.split("\n")
-    setup(name='fluxstyle',
+    setup(
+        name='fluxstyle',
         version='1.2',
         description=doclines[0],
         author='Michael Rice',
         author_email='errr@errr-online.com',
-        url='http://fluxstyle.berlios.de/',
+        url='https://github.com/michaelrice/fluxStyle/',
         packages=['fluxstyle'],
         data_files=[
-          (DATADIR+'/fluxstyle/images',
-            ['images/fluxmetal.png',
-              'images/mini-fluxbox6.png',
-              'images/none.jpg'
-            ]
-          ),
-          (DATADIR+'/fluxstyle/glade',['images/main.glade']),
-          (BINDIR,['fluxStyle']),
-          (DATADIR+'/fluxstyle/docs',
-              ['docs/README','docs/LICENSE','docs/Changelog'])
-          ]
-        )
+            (
+                DATADIR + '/fluxstyle/images', [
+                    'images/fluxmetal.png',
+                    'images/mini-fluxbox6.png',
+                    'images/none.jpg'
+                ]
+            ),
+            (DATADIR + '/fluxstyle/glade', ['images/main.glade']),
+            (BINDIR, ['fluxStyle']),
+            (DATADIR + '/fluxstyle/docs',
+            ['docs/README', 'docs/LICENSE', 'docs/Changelog'])
+        ]
+    )
+
+
 if __name__ == "__main__":
     main()
